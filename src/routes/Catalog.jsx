773591333ea75f9f68c6d3652ad780c769f9d159
Яков Носков <jakovnoskov@hotmail.com>
2022-10-12 03:12:42 +0600

    commit on git pages

--- src/routes/Catalog.jsx
+++ src/routes/Catalog.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Сategories from '../components/Сategories'
 import Sort from '../components/Sort'
 import PizzaBlock from '../components/PizzaBlock'
 import { Skeleton } from '../components/PizzaBlock/Skeleton'
-import pizzas from '../assets/db.json'
+import pizzasJson from '../assets/db.json'
 
 export default function Catalog() {
+  const [categoryId, setCategoryId] = useState(0)
+
+
+  const status = ''
+  const pizzas = pizzasJson.map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+  const skeletons = [...new Array(6)].map((_, index) => <Skeleton key={index} />);
+
+  const onChangeCategory = (index) => {
+    setCategoryId(index)
+  }
+
   return (
   <div className='content'>
     <div className='container'>
       <div className='content__top'>
-        <Сategories/>
-        <Sort/>
+        <Сategories value={categoryId} onChangeCategory={onChangeCategory}/>
+        <Sort value={sort}/>
       </div>
       <h2 className='content__title'>Все пиццы</h2>
-      <div className='content__items'>
-        {pizzas.map((item, i) => <PizzaBlock key={i + Number(item.id)} { ...item}/>)}
-      </div>
+      <div className="content__items">{status === 'loading' ? skeletons : pizzas}</div>
     </div>
   </div>
   )
